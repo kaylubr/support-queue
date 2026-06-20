f578e1cf0ff660d2616a6b3f40490583bfe52939
kaylubr <kylebreyes092005@gmail.com>
2026-06-20 21:05:04 +0800

    chore: rename prisma directory to lib & create .env.example file

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -4,9 +4,9 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "lib/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "lib/migrations",
   },
   datasource: {
     url: env("DATABASE_URL"),
