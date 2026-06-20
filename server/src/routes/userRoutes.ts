889b4849d234699fc9d03215c38dd4bc5d5a6318
kaylubr <kylebreyes092005@gmail.com>
2026-06-20 22:11:59 +0800

    feat: create endpoint for creating user

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllUsers } from '../services/userService';
+import { getAllUsers, createUser } from '../services/userService';
 
 const userRoutes = express.Router();
 
 userRoutes.get("/", getAllUsers);
+userRoutes.post("/register", createUser);
 
 export default userRoutes;
--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,5 +1,7 @@
 import type { RequestHandler } from "express";
+import { hash } from "bcrypt-ts";
 
+import { UserSchema } from "../schema";
 import { db } from "../config/db";
 
 export const getAllUsers: RequestHandler = async (_req, res, next) => {
@@ -7,8 +9,25 @@ export const getAllUsers: RequestHandler = async (_req, res, next) => {
     const users = await db.user.findMany();
     res.json(users);
   } catch (e: unknown) {
-    if (e instanceof Error) {
-      next(e);
-    }
+    next(e);
+  }
+};
+
+export const createUser: RequestHandler = async (req, res, next) => {
+  try {
+    const { name, email, password } = UserSchema.parse(req.body);
+
+    const saltRounds = 10;
+    const passwordHash = await hash(password, saltRounds);
+
+    const newUser = await db.user.create({
+      data: {
+        name, email, passwordHash
+      }
+    });
+
+    res.json(newUser);
+  } catch (e: unknown) {
+    next(e);
   }
 };
